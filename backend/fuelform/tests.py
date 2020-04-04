15e50856489d32c62e6c39b68055069254be4979
Andrew Vieira <5641660+AndrewVieira@users.noreply.github.com>
2020-04-03 22:22:29 -0500

    Some Finishing Touches

--- backend/fuelform/tests.py
+++ backend/fuelform/tests.py
@@ -16,7 +16,25 @@ from rest_framework import status
 class FuelFormTest(APITestCase):
 
     def test_create_fuel_data(self):
-        url = "http://127.0.0.1:8000/"
+        url = "http://127.0.0.1:8000/api/fuelform/"
         data = {"gallons":12.4, "delivery_address":"4418","delivery_date":"2020-01-01","suggested_price":1.99, "total_due":24.676}
         response = self.client.post(path=url, data=data, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_wrong_post_data(self):
+        url = "http://127.0.0.1:8000/api/fuelform/"
+        data = {}
+        response = self.client.post(path=url, data=data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def test_wrong_method(self):
+        url = "http://127.0.0.1:8000/api/fuelform/"
+        data = {"gallons":12.4, "delivery_address":"4418","delivery_date":"2020-01-01","suggested_price":1.99, "total_due":24.676}
+        response = self.client.get(path=url)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_post_path_request(self):
+        url = "http://127.0.0.1:8000/api/fuelform"
+        request= self.client.post(path=url)
+        self.assertEqual(request.status_code, status.HTTP_301_MOVED_PERMANENTLY)
+
