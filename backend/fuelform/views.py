6472aed138834704f0e07a4ea606d1e1bb546b9d
Nekruz Vatanshoev <nekruz.vatanshoev@outlook.com>
2020-04-21 12:42:36 -0500

    Updating views.py

--- backend/fuelform/views.py
+++ backend/fuelform/views.py
@@ -7,6 +7,7 @@ from rest_framework import viewsets
 from rest_framework import status
 from .serializer import FuelFormSerializer
 from .models import Fuel
+from clientprofile.models import ClientProfile
 
 class FuelFormView(viewsets.ModelViewSet):
     serializer_class = FuelFormSerializer
@@ -48,3 +49,50 @@ class FuelFormView(viewsets.ModelViewSet):
             print(e)
             return HttpResponse(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
+
+
+class CalcPriceFuelForm(viewsets.ViewSet):
+    def create(self, request, *args, **kwargs):
+        print(request.user)
+        print(request.data)
+
+        gallons = int(request.data["gallons"])
+        month = int(request.data["month"])
+        client = ClientProfile.objects.get(user=request.user)
+        location = client.state
+        profile = Fuel.objects.filter(user=request.user)
+
+        print("Gallons:",gallons)
+        print("Month:",month)
+        print("Location:",location)
+        print("Prev:",len(profile))
+
+        profit_factor = 0.1
+        month_factor = 0.03
+        prev_factor = 0.0
+        location_factor = 0.04
+        gallons_factor = 0.03
+        may = 4
+        september = 8
+
+        if location =="TX":
+            location_factor = 0.02
+
+
+        if len(profile)!=0:
+            prev_factor = -0.01
+
+        if gallons > 1000:
+            gallons_factor = 0.02
+
+        if month> may and month < september:
+            month_factor = 0.04
+
+        total_factors = 1.50+(profit_factor+location_factor+month_factor+prev_factor+gallons_factor)*1.50
+        total_price = gallons * total_factors
+        price = {}
+        price["price"] = total_factors
+        price["total_price"] = total_price
+
+
+        return response.Response(data=price)
