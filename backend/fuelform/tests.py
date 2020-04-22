e30e3f4767c3fc15888d1ae39c4748f59a5b59f9
Andrew Vieira <5641660+AndrewVieira@users.noreply.github.com>
2020-04-22 09:24:15 -0500

    Merging

--- backend/fuelform/tests.py
+++ backend/fuelform/tests.py
@@ -1,7 +1,10 @@
-from django.test import TestCase
-
+from django.test import TestCase, Client
+from rest_framework.authtoken.models import Token
 from rest_framework.test import APIClient, APITestCase
 from rest_framework import status
+from clientprofile.models import ClientProfile
+from django.contrib.auth.models import User
+from rest_framework.test import force_authenticate
 # Create your tests here.
 
 # class SmokeTest(TestCase):
@@ -38,3 +41,113 @@ class FuelFormTest(APITestCase):
         request= self.client.post(path=url)
         self.assertEqual(request.status_code, status.HTTP_301_MOVED_PERMANENTLY)
 
+    def test_price(self):
+        user = User.objects.create_user(username="bobby", password="PAZZWERD99")
+        client = APIClient()
+        response = client.login(username="bobby", password="PAZZWERD99")
+        client.force_authenticate(user=user)
+        profile = ClientProfile.objects.create(user=user)
+        profile.username = "Bobby"
+        profile.fullname = "Billy Bob"
+        profile.address1 = "123 Street"
+        profile.address2 = ""
+        profile.city = "Houston"
+        profile.state = "TX"
+        profile.zipcode = "77777"
+        profile.validated = True
+        profile.save()
+        url = "http://127.0.0.1:8000/api/price/"
+        data = {"gallons":50, "month":3}
+        response= client.post(path=url, data=data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_price_negative(self):
+        user = User.objects.create_user(username="bobby", password="PAZZWERD99")
+        client = APIClient()
+        response = client.login(username="bobby", password="PAZZWERD99")
+        client.force_authenticate(user=user)
+        profile = ClientProfile.objects.create(user=user)
+        profile.username = "Bobby"
+        profile.fullname = "Billy Bob"
+        profile.address1 = "123 Street"
+        profile.address2 = ""
+        profile.city = "Houston"
+        profile.state = "TX"
+        profile.zipcode = "77777"
+        profile.validated = True
+        profile.save()
+        url = "http://127.0.0.1:8000/api/price/"
+        data = {"gallons":-50, "month":-3}
+        response= client.post(path=url, data=data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_price_check_result_spring(self):
+        user = User.objects.create_user(username="bobby", password="PAZZWERD99")
+        client = APIClient()
+        response = client.login(username="bobby", password="PAZZWERD99")
+        client.force_authenticate(user=user)
+        profile = ClientProfile.objects.create(user=user)
+        profile.username = "Bobby"
+        profile.fullname = "Billy Bob"
+        profile.address1 = "123 Street"
+        profile.address2 = ""
+        profile.city = "Houston"
+        profile.state = "TX"
+        profile.zipcode = "77777"
+        profile.validated = True
+        profile.save()
+        url = "http://127.0.0.1:8000/api/price/"
+        data = {"gallons":50, "month":3}
+        response= client.post(path=url, data=data, format="json")
+        dict = {'price':1.77, 'total_price':88.5}
+        self.assertEqual(response.data, dict)
+
+    def test_price_check_result_summer(self):
+        user = User.objects.create_user(username="bobby", password="PAZZWERD99")
+        client = APIClient()
+        response = client.login(username="bobby", password="PAZZWERD99")
+        client.force_authenticate(user=user)
+        profile = ClientProfile.objects.create(user=user)
+        profile.username = "Bobby"
+        profile.fullname = "Billy Bob"
+        profile.address1 = "123 Street"
+        profile.address2 = ""
+        profile.city = "Houston"
+        profile.state = "TX"
+        profile.zipcode = "77777"
+        profile.validated = True
+        profile.save()
+        url = "http://127.0.0.1:8000/api/price/"
+        data = {"gallons":50, "month":6}
+        response= client.post(path=url, data=data, format="json")
+        dict = {'price':1.7850000000000001, 'total_price':89.25}
+        self.assertEqual(response.data, dict)
+
+        def test_price_check_result_multiple(self):
+            user = User.objects.create_user(username="bobby", password="PAZZWERD99")
+            client = APIClient()
+            response = client.login(username="bobby", password="PAZZWERD99")
+            client.force_authenticate(user=user)
+            profile = ClientProfile.objects.create(user=user)
+            profile.username = "Bobby"
+            profile.fullname = "Billy Bob"
+            profile.address1 = "123 Street"
+            profile.address2 = ""
+            profile.city = "Houston"
+            profile.state = "TX"
+            profile.zipcode = "77777"
+            profile.validated = True
+            profile.save()
+            Fuel.objects.create(
+                user=user,
+                gallons=7,
+                delivery_address=profile.address1,
+                delivery_date="03",
+                suggested_price=80,
+                total_due=9001
+                )
+            url = "http://127.0.0.1:8000/api/price/"
+            data = {"gallons":50, "month":6}
+            response= client.post(path=url, data=data, format="json")
+            dict = {'price':1.7850000000000001, 'total_price':89.25}
+            self.assertEqual(response.data, dict)
